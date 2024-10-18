4500d0294e59a9c1b0a854da2212bf63e1b356bb
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-18 22:38:03 +0800

    Fix up homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			Test
+			<h1>Revision Notes</h1>
+			<button className={styles.option} id={styles.IB}>IB</button>
+			<button className={styles.option} id={styles.GCSE}>GCSE</button>
 		</div>
 	);
 }
