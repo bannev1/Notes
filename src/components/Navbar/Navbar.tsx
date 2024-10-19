f7c0e818cff2a994d6ce8a6490e2f1f801d366ac
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-19 18:22:52 +0800

    Add github logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import HomeIcon from '../../images/HomeIcon.svg'
+import GithubLogo from '../../images/GithubLogo.svg'
 
 function Navbar() {
   return (
@@ -8,7 +9,7 @@ function Navbar() {
 		<div id="Navbar">
 			<div><a href="/"><Image src={HomeIcon} alt="Home Icon"/></a></div>
 			<div>
-				<a href="https://github.com/realhuman101/Notes/" target="_blank"></a>
+				<a href="https://github.com/realhuman101/Notes/" target="_blank"><Image src={GithubLogo} alt=""/></a>
 			</div>
 		</div>
 		<div id="navFill"></div>
