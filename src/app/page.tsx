4918160f6ee146eb1d22e196f3e808a840502f6a
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-19 18:18:32 +0800

    Fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
+import SideNav from "@/components/SideNav/SideNav";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<h1>Revision Notes</h1>
-			<button className={styles.option} id={styles.IB}>IB</button>
-			<button className={styles.option} id={styles.GCSE}>GCSE</button>
+			<SideNav/>
+			<h3 id={styles.pageCredit}>Written and Developed by <a data-content="Valentina Banner" href="https://realhuman101.github.com">Valentina Banner</a></h3>
+			<p>Use the side navigation menu to navigate through the notes</p>
 		</div>
 	);
 }
