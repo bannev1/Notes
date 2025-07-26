4a604752e125eec8359990bedef5602ed1e1a172
Valentina Banner <78287030+bannev1@users.noreply.github.com>
2025-07-26 16:56:02 +0800

    change site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 		<div className={styles.page}>
 			<h1>Revision Notes</h1>
 			<SideNav/>
-			<h3 id={styles.pageCredit}>Written and Developed by <a data-content="Valentina Banner" href="https://realhuman101.github.com">Valentina Banner</a></h3>
+			<h3 id={styles.pageCredit}>Written and Developed by <a data-content="Valentina Banner" href="https://valentinabanner.com">Valentina Banner</a></h3>
 			<p>Use the side navigation menu to navigate through the notes</p>
 		</div>
 	);
